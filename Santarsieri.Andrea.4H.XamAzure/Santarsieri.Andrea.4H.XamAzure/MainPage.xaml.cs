2b5eea4fe66bc0600a3e22f96b364fb05d843db7
andreasantarsieri <77383945+andreasantarsieri@users.noreply.github.com>
2021-05-12 10:04:44 +0200

    Primo Commit

---
+++ Santarsieri.Andrea.4H.XamAzure/Santarsieri.Andrea.4H.XamAzure/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using Santarsieri.Andrea._4H.XamAzure.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Santarsieri.Andrea._4H.XamAzure
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            List<Studente> elenco = new List<Studente>();
+
+            try
+            {
+                HttpClient client = new HttpClient();
+                string response = await client.GetStringAsync("https://flr.azurewebsites.net/studenti");
+                elenco = JsonConvert.DeserializeObject<List<Studente>>(response);
+            }
+            catch (Exception err)
+            {
+                await DisplayAlert("Ocio!!", err.Message, "Ok");
+            }
+            //elenco.Add(new Studente { Nome="Andrea" , Cognome="Santarsieri" });
+            //elenco.Add(new Studente { Nome = "Fabio", Cognome = "Corbelli" });
+            //elenco.Add(new Studente { Nome = "Maurizio", Cognome = "Conti" });
+
+            lvStudenti.ItemsSource = elenco;
+        }
+    }
+
+}
---
+++ Santarsieri.Andrea.4H.XamAzure/Santarsieri.Andrea.4H.XamAzure/Models/Studente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Santarsieri.Andrea._4H.XamAzure.Models
+{
+    class Studente
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+
+    }
+}
